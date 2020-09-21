b1bca538a7ad1625d9b1d8c0322d766e69dc0e91
Ludovic Räss <ludovic.rass@gmail.com>
2020-09-21 22:31:19 +0200

    Visualisation script to load the binary output data produced for a specific "run_id" Visu happens in casted Float32 format for a lighter world

---
+++ scripts/vizme.jl
@@ -0,0 +1,40 @@
+const Dat = Float32
+using Printf, Statistics, LinearAlgebra, DelimitedFiles, Plots
+
+run_id = "run_ds_40"
+
+outdir = "../output/$run_id"
+function load_array(A, A_name, extension, isave, outd)
+    fid=open("$outd/$(isave)_$(A_name).$(extension)", "r"); read!(fid, A); close(fid)
+end
+
+Info = readdlm("$outdir/0_infos.inf")
+
+Lx    = Info[1]
+Ly    = Info[2]
+nx    = Int(Info[3])
+ny    = Int(Info[4])
+isave = Int(Info[5])
+err   = Info[6]
+
+Mask = zeros(Dat, nx, ny)
+xcp  = zeros(Dat, nx, 1 )
+ycp  = zeros(Dat, ny, 1 )
+F    = zeros(Dat, nx, ny)
+
+load_array(Mask, "M"   , "res", 0, outdir)
+load_array(xcp , "xcp" , "res", 0, outdir)
+load_array(ycp , "ycp" , "res", 0, outdir)
+
+load_array(F   , "F"   , "res", isave, outdir)
+
+# visu
+F[Mask.==0] .= NaN
+xcp = xcp[:,1]
+ycp = ycp[:,1]
+
+plt = heatmap(xcp, ycp, log10.(F'), aspect_ratio=1, xlims=(xcp[1], xcp[end]), ylims=(ycp[1], ycp[end]), c=:viridis, framestyle=:box, title="log10 ||Flux|| (ϵ=$err)")
+
+savefig(plot(plt, dpi=300), joinpath(@__DIR__, "$outdir/o$(length(xcp))_$isave.png"))
+
+display(plt)
