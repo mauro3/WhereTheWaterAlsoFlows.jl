a84fe652673a24bb94468b8df45bc72cc1f9a350
Ludovic Räss <ludovic.rass@gmail.com>
2020-09-21 22:26:26 +0200

    Improved I/O and GPU capabilities, including: - I/O to ../output/"run_id" where "run_id" can be selected on a per-run basis - data padding upon upload to fit multiples of 32, 8 [x,y] as needed by ParallelStencil. Issue opened on CUDA.jl to fix this (see # DEBUG for more details) - final post-processing and fig save upon successful run completion

--- scripts/antarctica_ludo.jl
+++ scripts/antarctica_ludo.jl
@@ -3,14 +3,18 @@ using Pkg
 Pkg.activate(".")
 using NCDatasets, Plots, Parameters
 
+!ispath("../data") && mkdir("../data")
+!ispath("../output") && mkdir("../output")
+
 # smooth_surface = 2000.0 # smoothing radius, in meters
-nsmooth  = 6
+nsmooth  = 2
+
+run_id = "run_ds_40" # DEBUG: define the output directory within ../output/
+
+!ispath("../output/$run_id") && mkdir("../output/$run_id")
 
 const rw = 1000.0
 const ri = 910.0
-
-!ispath("../data") && mkdir("../data")
-!ispath("../output") && mkdir("../output")
 
 url = "file://" * homedir() * "/itet-stor/glazioarch/GlacioData/BedMachine_Antarctica/168596330/BedMachineAntarctica_2019-11-05_v01.nc"
 # url = "file://" * "/Volumes/luraess/glazioarch/GlacioData/BedMachine_Antarctica/168596330/BedMachineAntarctica_2019-11-05_v01.nc"
@@ -23,22 +27,35 @@ else
     joinpath("../data", splitdir(url)[2])
 end
 
-# if !(@isdefined ds)
+# if !(@isdefined ds) # DEBUG: should be done differently to allow for changing ds while the file already downloaded
     print("Loading NC file ... ")
     ds = NCDataset(filename)
 
     # downscale = 2 finest possible on 16GB
     # downscale = 1 ok on 32GB ram (needs around 16GB)
-    downscale = 20
+    downscale = 40
 
     x  = ds["x"][1:downscale:end]
     y  = ds["y"][1:downscale:end]
     x  = x[1]:x[2]-x[1]:x[end]
     y  = y[1]:y[2]-y[1]:y[end]
-    mask_   = ds["mask"][1:downscale:end,1:downscale:end]
+    mask_   = ds["mask"][1:downscale:end, 1:downscale:end]
     mask    = (mask_.==2) .| (mask_.==4) .| (mask_.==1) # route water over(under): grounded ice or lake Vostok or ice-free-land
-    surface = ds["surface"][1:downscale:end,1:downscale:end]
-    bed     = convert(Matrix{Float32}, ds["bed"][1:downscale:end,1:downscale:end])
+    surface = ds["surface"][1:downscale:end, 1:downscale:end]
+    bed     = convert(Matrix{Float32}, ds["bed"][1:downscale:end, 1:downscale:end])
+
+    # Pad the data to make it GPU compatible with good performance # DEBUG: do this until ParallelStencil is fixed: see https://github.com/samo-lin/ParallelStencil.jl/issues/42
+    olx = rem(length(x), 32)
+    oly = rem(length(y), 8 )
+    x1=1; xE=0; if (olx!=0) x1 = Int(ceil(olx/2)); xE = olx-x1+1; end
+    y1=1; yE=0; if (oly!=0) y1 = Int(ceil(oly/2)); yE = oly-y1+1; end
+
+    x  = x[x1:end-xE]
+    y  = y[y1:end-yE]
+    mask    = mask[x1:end-xE, y1:end-yE]
+    surface = surface[x1:end-xE, y1:end-yE]
+    bed     = bed[x1:end-xE, y1:end-yE]
+
     println("done")
 # end
 println("grid size: nx=$(length(x)), ny=$(length(y))")
@@ -86,11 +103,15 @@ surf[.!mask] .= NaN
 using WhereTheWaterAlsoFlows
 #const WWAF = WhereTheWaterAlsoFlows
 
-@time D, F, xcp, ycp = flow_routing2D(x, reverse(y), bed, surface.-bed, mask, mask*1)
+@time D, F, xcp, ycp = flow_routing2D(x, reverse(y), bed, surface.-bed, mask, mask*1, outdir="../output/$run_id")
 
 # visu
 F[.!mask] .= NaN
-display(heatmap(xcp, ycp, log10.(F'), aspect_ratio=1, xlims=(xcp[1], xcp[end]), ylims=(ycp[1], ycp[end]), c=:viridis, framestyle=:box, title="log10 ||Flux||"))
 
+plt = heatmap(xcp, ycp, log10.(F'), aspect_ratio=1, xlims=(xcp[1], xcp[end]), ylims=(ycp[1], ycp[end]), c=:viridis, framestyle=:box, title="log10 ||Flux||")
+
+savefig(plot(plt, dpi=300), joinpath(@__DIR__, "../output/$run_id/o$(length(x))_final.png"))
+
+display(plt)
 
 nothing
